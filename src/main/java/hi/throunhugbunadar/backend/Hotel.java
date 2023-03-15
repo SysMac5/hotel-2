421dddcf509d2c54a7ea38c253c1b6ad9822bd43
Gyda Perla Bjarnadottir <gpb6@hi.is>
2023-03-15 17:22:31 +0000

    Búið til föll fyrir Hotel og HotelRooms klasa. Klasinn HotelroomList búinn til.

--- src/main/java/hi/throunhugbunadar/backend/Hotel.java
+++ src/main/java/hi/throunhugbunadar/backend/Hotel.java
@@ -14,4 +14,37 @@ public class Hotel {
     private List<HotelRooms> hotelRoomsList;
     private int stars;
     private String location;
+
+    public Hotel(String name, String info, java.util.List<Image> imageList, List<HotelRooms> hotelRoomsList, int stars, String location) {
+        this.name = name;
+        this.info = info;
+        this.imageList = imageList;
+        this.hotelRoomsList = hotelRoomsList;
+        this.stars = stars;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public List<Image> getImageList() {
+        return imageList;
+    }
+
+    public List<HotelRooms> getHotelRoomsList() {
+        return hotelRoomsList;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
