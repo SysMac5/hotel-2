049f73a58febedf5a259a1b0b7566e29b3aaeaa4
Gyda Perla Bjarnadottir <gpb6@hi.is>
2023-04-08 20:18:01 +0000

    Bjó til interface fyrir HotelRepository. Method documentation fyrir HotelroomList.

--- src/main/java/hi/throunhugbunadar/backend/HotelroomList.java
+++ src/main/java/hi/throunhugbunadar/backend/HotelroomList.java
@@ -1,10 +1,9 @@
 package hi.throunhugbunadar.backend;
 
-import java.awt.*;
 import java.util.ArrayList;
 
 /**
- * Listi af hótelherbergjum.
+ * Listi af hótelherbergjum sem hægt er að sía eftir verði og stjörnum hótela.
  */
 
 public class HotelroomList {
@@ -14,10 +13,20 @@ public class HotelroomList {
     private int minStars = 1;
     private int maxStars = 5;
 
+    /**
+     * Smiður sem tekur inn lista af hótelherbergjum.
+     *
+     * @param hotelroomList Listi af hótelherbergjum.
+     */
     public HotelroomList(ArrayList<HotelRooms> hotelroomList) {
         this.hotelroomList = hotelroomList;
     }
 
+    /**
+     * Skilar ArrayList með hótelherbergjum sem uppfylla núverandi skilyrði.
+     *
+     * @return ArrayList með hótelherbergjum sem uppfylla núverandi skilyrði.
+     */
     public ArrayList<HotelRooms> getList() {
         ArrayList<HotelRooms> filteredList = new ArrayList<>();
 
@@ -31,16 +40,30 @@ public class HotelroomList {
         return filteredList;
     }
 
+    /**
+     * Stillir lágmarks- og hámarksverð á hótelherbergjum.
+     *
+     * @param minPrice lágmarksverð á hótelherberjum.
+     * @param maxPrice hámarksverð á hótelherbergjum.
+     */
     public void filterByPrice(int minPrice, int maxPrice) {
         this.minPrice = minPrice;
         this.maxPrice = maxPrice;
     }
 
+    /**
+     * Stillir lágmarks- og hámarksstjörnufjölda á hótelum.
+     * @param minStars lágmarksstjörnufjöldi á hótelum.
+     * @param maxStars hámarksstjörnufjöldi á hótelum.
+     */
     public void filterByStars(int minStars, int maxStars) {
         this.minStars = minStars;
         this.maxStars = maxStars;
     }
 
+    /**
+     * Endurræsir stillingar á lágmarki og hámarki bæði á verði hótelherbergja og stjörnufjölda hótela.
+     */
     public void restartFilter() {
         minPrice = 0;
         maxPrice = Integer.MAX_VALUE;
