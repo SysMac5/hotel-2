0ef6ff71bb994249739f0d227da8f666f8e4f397
Gyda Perla Bjarnadottir <gpb6@hi.is>
2023-04-11 11:58:23 +0000

    Lýsingar á föll í HotelRepository.

---
+++ src/main/java/hi/throunhugbunadar/backend/HotelRepository.java
@@ -0,0 +1,27 @@
+package hi.throunhugbunadar.backend;
+
+import java.util.ArrayList;
+
+public class HotelRepository implements iHotelRepository {
+    /**
+     * Finnur og skilar lista af hótelum sem uppfylla leit m.t.t. nafn hótels.
+     *
+     * @param hotelName Nafn á hóteli sem leitað er eftir.
+     * @return ArrayList með hótelum sem uppfylla leit.
+     */
+    @Override
+    public ArrayList<Hotel> searchForHotel(String hotelName) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Finnur og skilar HotelroomList hlut sem inniheldur hótelherbergi sem uppfylla gefin leitarskilyrði.
+     *
+     * @param criteria Leitarskilyrði sem eru notuð til að leita að hótelherbergjum.
+     * @return ArrayList með hótelherbergjum sem uppfylla gefin leitarskilyrði.
+     */
+    @Override
+    public ArrayList<HotelRooms> searchByCriteria(Criteria criteria) {
+        throw new UnsupportedOperationException();
+    }
+}
