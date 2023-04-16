08d3ca1d2cc35f7ce1c0d0803bfd8b4d0dda3a98
SysMac5 <sigurthor.maggi@gmail.com>
2023-04-16 16:57:52 +0000

    UserRepository klárað.

--- src/main/java/hi/throunhugbunadar/backend/UserRepository.java
+++ src/main/java/hi/throunhugbunadar/backend/UserRepository.java
@@ -56,7 +56,27 @@ public class UserRepository implements iUserRepository {
      * @param user uppfærður notandi
      * @return hvort breyting hafi heppanst
      */
-    public boolean updateUser(User user) { // óklárað ! !
-        throw new UnsupportedOperationException();
+    public boolean updateUser(User user) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("UPDATE users SET name = ?, email = ?, phone_number = ?, password = ? WHERE username = ?");
+            statement.setString(1,user.getName());
+            statement.setString(2,user.getEmail());
+            statement.setString(3,user.getPhoneNumber());
+            statement.setString(4,user.getPassword());
+            statement.setString(5,user.getUsername());
+            statement.executeUpdate();
+
+            statement = connection.prepareStatement("UPDATE payment_info SET card_number = ?, month_valid = ?, year_valid = ?, cvv = ? WHERE id = (SELECT payment_info_id FROM users WHERE username = ?)");
+            statement.setString(1,user.getPaymentInfo().getCardNumber());
+            statement.setString(2,user.getPaymentInfo().getMonthValid());
+            statement.setString(3,user.getPaymentInfo().getYearValid());
+            statement.setString(4,user.getPaymentInfo().getCvv());
+            statement.setString(5,user.getUsername());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
     }
 }
